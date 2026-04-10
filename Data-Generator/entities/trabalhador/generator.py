75151e4c6a60dfbcd39400c27fd1e952ca37251b
Mariana <marianalbferreira27@gmail.com>
2026-04-10 12:22:40 +0100

    feat: Add generator scripts for 'cidadao' and clean up 'trabalhador' entity

--- Data-Generator/entities/trabalhador/generator.py
+++ Data-Generator/entities/trabalhador/generator.py
@@ -2,8 +2,3 @@ from faker import Faker
 
 faker = Faker('pt_PT')
 
-for _ in range(10):
- generatedAddress = faker.address()
- generatedNames = faker.name()
- # print(generatedNames)
-print(generatedAddress)
